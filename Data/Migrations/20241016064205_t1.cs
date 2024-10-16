7b793d022d4a4ca132bac069734d82c1f3ba6038
Nadia-wh <64726301+Nadia-wh@users.noreply.github.com>
2024-10-16 15:07:26 +0530

    Add files via upload

---
+++ Data/Migrations/20241016064205_t1.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace demo_analyser.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class t1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "AnswerSheets",
+                newName: "Upload_Status");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "AnswerSheets",
+                newName: "AP_Id");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AP_Link",
+                table: "AnswerSheets",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AP_Link",
+                table: "AnswerSheets");
+
+            migrationBuilder.RenameColumn(
+                name: "Upload_Status",
+                table: "AnswerSheets",
+                newName: "Name");
+
+            migrationBuilder.RenameColumn(
+                name: "AP_Id",
+                table: "AnswerSheets",
+                newName: "Id");
+        }
+    }
+}
---
+++ Models/AnswerSheets.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace demo_analyser.Models
+{
+    public class AnswerSheets
+    {
+        [Key]
+        public int AP_Id { get; set; } // primary key for each answersheet
+
+        [Required]
+        public string AP_Link { get; set; } //the saved url of file in each blob
+
+        [Required]
+        public string Upload_Status { get; set; } // the status of file after uploading 
+    }
+}
